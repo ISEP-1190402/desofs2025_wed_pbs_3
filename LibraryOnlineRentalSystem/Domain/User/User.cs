1e8a2190fab56a86ba18f134678477a796bcb7a0
Carla Barbosa <1200928@isep.ipp.pt>
2025-05-17 20:28:20 +0100

    #30 modifications in user and create userservice

--- LibraryOnlineRentalSystem/Domain/User/User.cs
+++ LibraryOnlineRentalSystem/Domain/User/User.cs
@@ -5,67 +5,62 @@ namespace LibraryOnlineRentalSystem.Domain.User;
 
 public class User : Entity<UserId>, IAggregateRoot
 {
-    public User(Name name, Email email, RoleId roleId, UserName userName, PhoneNumber phoneNumber, NIF nif,
-        Biography biography)
+    protected User() { }
+    public User(string id, string name, string email, string roleId, string userName, string phoneNumber, string nif, string biography)
     {
-        if (roleId == null)
-            throw new BusinessRulesException("Every user requires a role.");
-
-        Id = new UserId(Guid.NewGuid());
-        Name = (Name)name.Clone();
-        UserName = (UserName)userName.Clone();
-        Email = (Email)email.Clone();
-        PhoneNumber = (PhoneNumber)phoneNumber.Clone();
-        RoleId = roleId;
-        Nif = (NIF)nif.Clone();
-        Biography = (Biography)biography.Clone();
+        Id = new UserId(id);
+        Name = new Name(name);
+        Email = new Email(email);
+        RoleId = new RoleId(roleId);
+        UserName = new UserName(userName);
+        PhoneNumber = new PhoneNumber(phoneNumber);
+        Nif = new NIF(nif);
+        Biography = new Biography(biography);
     }
-
-    public User(string id, Name name, Email email, RoleId roleId, UserName userName, PhoneNumber phoneNumber, NIF nif,
-        Biography biography)
+    
+    public User(string name, string email, string roleId, string userName, string phoneNumber, string nif, string biography)
     {
-        if (roleId == null)
-            throw new BusinessRulesException("Every user requires a role.");
-
-        Id = new UserId(Guid.Parse(id));
-        Name = (Name)name.Clone();
-        UserName = (UserName)userName.Clone();
-        Email = (Email)email.Clone();
-        PhoneNumber = (PhoneNumber)phoneNumber.Clone();
-        RoleId = roleId;
-        Nif = (NIF)nif.Clone();
-        Biography = (Biography)biography.Clone();
+        Id = new UserId(Guid.NewGuid());
+        Name = new Name(name);
+        Email = new Email(email);
+        RoleId = new RoleId(roleId);
+        UserName = new UserName(userName);
+        PhoneNumber = new PhoneNumber(phoneNumber);
+        Nif = new NIF(nif);
+        Biography = new Biography(biography);
     }
 
     public Name Name { get; private set; }
-
+    public Email Email { get; private set; }
+    public RoleId RoleId { get; private set; }
     public UserName UserName { get; private set; }
-
     public PhoneNumber PhoneNumber { get; private set; }
-
     public NIF Nif { get; private set; }
-
     public Biography Biography { get; private set; }
 
-    public Email Email { get; private set; }
-
-    public RoleId RoleId { get; private set; }
-
-
-    public void ChangeBiography(Biography biography)
+    // Métodos de alteração segura (mutações controladas)
+    public void ChangeBiography(string biography)
     {
-        Biography = (Biography)biography.Clone();
+        Biography = new Biography(biography);
     }
 
-    public void ChangeEmail(Email email)
+    public void ChangeEmail(string email)
     {
-        Email = (Email)email.Clone();
+        Email = new Email(email);
     }
 
-    public void ChangeRoleId(RoleId roleId)
+    public void ChangePhoneNumber(string phoneNumber)
     {
-        if (roleId == null)
-            throw new BusinessRulesException("Every user requires a role.");
-        RoleId = roleId;
+        PhoneNumber = new PhoneNumber(phoneNumber);
+    }
+
+    public void ChangeUserName(string userName)
+    {
+        UserName = new UserName(userName);
+    }
+
+    public void ChangeRoleId(string roleId)
+    {
+        RoleId = new RoleId(roleId);
     }
 }
