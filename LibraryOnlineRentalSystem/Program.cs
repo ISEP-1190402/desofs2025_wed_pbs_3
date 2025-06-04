d86d513c72d90cf13628d496ff5bb406681da236
carlos <carlos@carloss-MacBook-Pro.local>
2025-06-04 19:35:19 +0100

    add comment test pipeline

--- LibraryOnlineRentalSystem/Program.cs
+++ LibraryOnlineRentalSystem/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 namespace LibraryOnlineRentalSystem
 {
     public class Program
+    // test pipeline
     {
         public static void Main(string[] args)
         {
