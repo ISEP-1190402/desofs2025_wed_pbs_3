29677d3b893cbf83de0630b807f9938372130160
= <=>
2025-06-10 16:41:32 +0100

    teste do pull

--- LibraryOnlineRentalSystem/Domain/Common/IAuditLogger.cs
+++
@@ -1,6 +0,0 @@
-namespace LibraryOnlineRentalSystem.Domain.Common;
-
-public interface IAuditLogger
-{
-    Task LogAsync(string message, string category);
-}
--- LibraryOnlineRentalSystem/Domain/Common/IWorkUnity.cs
+++
@@ -1,6 +0,0 @@
-namespace LibraryOnlineRentalSystem.Domain.Common;
-
-public interface IWorkUnity
-{
-    Task<int> CommitAsync();
-}
